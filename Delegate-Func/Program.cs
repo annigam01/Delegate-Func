18e99f548594e370e7892e3ebcefda72367d36a8
Anand Nigam <annigam01@hotmail.com>
2017-03-22 13:50:23 +0530

    Add project files.

---
+++ Delegate-Func/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate_Func
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        // the func<input variable type, output variable type> is a c# feature.
+        // using this method, you dont need to Create delegate seperatly first, you can declare and define the function all in one line.
+        //
+        //
+
+            Func<int, int> A = x => x * x;
+            Console.WriteLine(A(3));
+            Console.Read();
+        }
+    }
+}
